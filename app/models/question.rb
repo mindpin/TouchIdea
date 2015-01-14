bf0cdd13aca954f04ac4982ddf315a591a9f885b
destinyd <destinyd_war@163.com>
2015-01-14 02:43:44 +0000

    clear voted users if create a new question

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,4 +13,10 @@ class Question
   def answered_by? user
     answers.map{|answer| answer.answered_by?(user)}.include?(true)
   end
+
+  protected
+  before_create :clear_voters_if_new_record
+  def clear_voters_if_new_record
+    vote.voted_users.delete_all if new_record?
+  end
 end
