406358f1841a93c2717eaa4f29a04431283f94dc
destinyd <destinyd_war@163.com>
2015-02-15 07:28:42 +0000

    fix devise sign in redirect to nil bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource_or_scope)
-    URI.parse(request.referer).path if request.referer
+    if request.referer
+      redirect_path = URI.parse(request.referer).path
+      return redirect_path if redirect_path != new_user_session_path
+    end
+    '/'
   end
 end
