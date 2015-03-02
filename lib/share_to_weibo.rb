fb24f46fe1bc17e0eca34fbaaf705b2ea43ece4a
destinyd <destinyd_war@163.com>
2015-03-02 08:08:43 +0000

    share to weibo pagination

--- lib/share_to_weibo.rb
+++ lib/share_to_weibo.rb
@@ -1,6 +1,6 @@
 class ShareToWeibo
   def initialize(args={})
-    
+    @users_count_per_share = (ENV['USERS_COUNT_PER_SHARE'] || 10).to_i
   end
 
   def post_statuses_for_vote_invite_users vote
@@ -9,11 +9,26 @@ class ShareToWeibo
     return false if token.nil?
     client = WeiboOAuth2::Client.new
     client.get_token_from_hash({:access_token=> token.token,:expires_at=> token.expires_at})
-    names = vote.invite_uids.map{ |uid| client.users.show_by_uid(uid).name }
+    @short_url = short_url client, vote
+    short_title = vote.title.block(25)
 
-    short = short_url client, vote
-    status = "【#{vote.title.block(25)}】 #{names.collect{|name| '@' + name}.join(' ')} 你们怎么看 #{short}"
-    client.statuses.update(status)
+    names = vote.invite_uids.map{ |uid| client.users.show_by_uid(uid).name }
+    
+    plus = 0
+    while !names.blank?
+      status = "【#{short_title}】 #{names.pop(@users_count_per_share).collect{|name| '!' + name}.join(' ')} 你们怎么看 #{@short_url}"
+      times = 0
+      begin
+        client.statuses.update(status)
+        p 'success share ' + status
+        sleep 1
+      rescue
+        times = times + 1
+        break if times > 2
+        sleep 3
+        retry
+      end
+    end
   end
 
   def short_url client, vote
