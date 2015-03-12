af6f3f189405245a450c64179da506ee9940e16c
destinyd <destinyd_war@163.com>
2015-03-12 08:46:33 +0000

    通过微博组件邀请好友

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,4 +5,9 @@ class Friendship
   field :name, type: String
   belongs_to :user, inverse_of: 'friendships'
   belongs_to :friend, class_name: 'User', inverse_of: 'friend_users', primary_key: :uid, foreign_key: :uid
+
+  validates_presence_of :uid
+  validates_presence_of :name
+
+  validates :user_id, uniqueness: {scope: [:uid]}, presence: true
 end
