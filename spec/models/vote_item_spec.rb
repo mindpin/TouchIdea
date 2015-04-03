febf7b47f92667bfe5f5c70b99f3f675bcb73785
destinyd <destinyd_war@163.com>
2015-04-03 08:09:37 +0000

    vote item spec

--- spec/models/vote_item_spec.rb
+++ spec/models/vote_item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe VoteItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+
+  describe "@user create @vote with @vote_item" do
+    before(:each) do
+      @user = create(:user)
+      @vote = create(:vote_with_vote_item, user: @user)
+      @vote_item = @vote.vote_items.first
+    end
+
+    it { @vote_item.should be_valid}
+    it { @vote_item.user.should == @user}
+  end
 end
