a08b4e4bb1992be9da5c5064751b3c1d13d68846
destinyd <destinyd_war@163.com>
2015-04-16 08:36:58 +0000

    热门，翻页与json显示格式

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -26,10 +26,20 @@ class VotesController < ApplicationController
   end
 
   def hot
-    @votes = Vote.hot.page(params[:page])
+    @votes = Vote.hot.page(params[:page]).per(10)
     respond_to do |format|
       format.html { render :index}
-      format.json { render json: @votes }
+      format.json do
+        votes_hash = @votes.map do |vote|
+          {
+            :id           => vote.id.to_s,
+            :title        => vote.title,
+            :joiner_count => vote.voted_users_count,
+            :options      => vote.vote_items.map{|vi|vi.title}
+          }
+        end
+        render(json: votes_hash)
+      end
     end
   end
 
