e6b1264fc3564f7c4c55219a273f5e81d7d6834f
destinyd <destinyd_war@163.com>
2015-04-16 07:49:36 +0000

    create vote_item with is_extra: true

--- app/controllers/vote_items_controller.rb
+++ app/controllers/vote_items_controller.rb
@@ -3,7 +3,7 @@ class VoteItemsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create, :praise]
   def create
     @vote = Vote.find params[:vote_id]
-    @vote_item = @vote.vote_items.create vote_item_params.merge(user: current_user)
+    @vote_item = @vote.vote_items.create vote_item_params.merge(user: current_user).merge(is_extra: true)
     respond_to do |format|
       format.json { render json: @vote_item.valid? ? @vote_item : @vote_item.errors}
       format.js
